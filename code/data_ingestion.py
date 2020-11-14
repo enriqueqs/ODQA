41217ee36b528b9dc51fcc894dae2a5f66ec8c7c
Enrique Quiroz <s193187@student.dtu.dk>
2020-11-14 15:04:22 +0100

    pickled data

--- code/data_ingestion.py
+++ code/data_ingestion.py
@@ -1,12 +1,13 @@
 from datasets import load_dataset, ReadInstruction
 import random
+import pickle
 random.seed(42)
 
 # From record 10 (included) to record 20 (excluded) of `train` split.
-train_10_20_ds = load_dataset('trivia_qa','rc', split=ReadInstruction('train', from_=10, to=20, unit='abs'))
+train_0_200_ds = load_dataset('trivia_qa','rc', split=ReadInstruction('train', from_=1, to=200, unit='abs'))
 
 # return search results
-train_10_20_ds[0]['search_results']
+# train_10_20_ds[0]['search_results']
 
 '''
 >>> train_10_20_ds[0].keys()
@@ -18,7 +19,9 @@ def search_result_from_question(data, n: int) -> dict:
     for question in data:
         q = question['question']
         qty_orig_source=len(question['search_results']['rank'])
-        if (n>qty_orig_source):
+        if (qty_orig_source < 3) :
+            continue
+        if (n > qty_orig_source):
             q_filling=n-qty_orig_source
             j=0
             sources={}
@@ -28,6 +31,8 @@ def search_result_from_question(data, n: int) -> dict:
                 ranks[i]=question['search_results']['rank'][i]
             while(q_filling>0):
                 l = random.randrange(len(data))
+                while(len(data[l]['search_results']['rank'])<3):
+                    l = random.randrange(len(data))
                 m = random.randrange(len(data[l]['search_results']['rank']))
                 sources[j+qty_orig_source]=data[l]['search_results']['search_context'][m]
                 ranks[j+qty_orig_source]=-1
@@ -41,9 +46,12 @@ def search_result_from_question(data, n: int) -> dict:
                 "ranks": ranks}
     return out
 
+reduced_dataset = search_result_from_question(train_0_200_ds, 10)
+data_path="/Users/enriqueqs/Github_repos/ODQA/data/processed"
+with open(data_path + '/data.pickle', 'wb') as f:
+    pickle.dump(reduced_dataset, f)
 
-oo = search_result_from_question(train_10_20_ds, 10)
-oo
+import sys
 '''
 >>> train_10_20_ds[0]['question']
 'Which innovation for the car was developed by Prince Henry of Prussia in 1911?'
